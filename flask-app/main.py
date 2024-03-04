943be59cea3b8b2c483371b56136bfbf7c9058d8
Hazem Khairy <hazem.hassan.kh@gmail.com>
2024-03-04 15:35:40 +0200

    feat: create a flask endpoint for object detection
    It saves the image to a local file, and returns the path

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,41 +1,60 @@
 import os
 import logging
-import time
+import datetime
 import cv2
+from nptyping import NDArray, Shape
 from flask_app.stream_loader import RTSPOpenCVStreamLoader
 from flask_app.exceptions import VideoCapError
 from flask_app.yolov5 import Model, Preprocessor, ModelLoader
 from flask_app.annotator import Annotator
 from flask import Flask
+from flask import request, jsonify
 
 # logging setup
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# stream loader setup
-# stream_loader = RTSPOpenCVStreamLoader(os.getenv("RTSP_URL"), os.getenv("THREAD_RETRY_INTERVAL"))
+
+# Initialize the Stream Loader
 stream_loader = RTSPOpenCVStreamLoader("rtsp://localhost:8554/stream", 5)
+# Load the model
+model = Model(ModelLoader.load_model("yolov5s.onnx"))
+# initialize the flask app
+app = Flask(__name__)
 
 
+@app.route("/object_detection")
+def object_detection():
+    # validate it's a GET request
+    if request.method != "GET":
+        return "Only GET requests are supported on this endpoint", 400
+    
+    # Try to get the next frame from the stream
+    try:
+        frame = stream_loader.load_frame()
+    except VideoCapError as e:
+        logger.error(f"Error when trying to load frame {e}")
+        return "Internal Server Error: Error when trying to load frame", 500
+    except Exception as e:
+        return str(e), 500
+    
+    # Preprocess the frame
+    preprocessed_frame, scale_factor = Preprocessor.preprocess(frame)
+    # pass it to the model
+    detected_objects = model.predict_and_get_detected_objects(preprocessed_frame)
+    # annotate a copy of the original frame
+    annotated_frame = Annotator(
+        detected_objects=detected_objects, 
+        original_image=frame, 
+        scale_factor=scale_factor
+    ).annotate()
+
+    # write the frame to a file
+    time_now = datetime.datetime.now().isoformat()
+    filename = f"annotated_frame_{time_now}.jpg"
+    # before writing using cv2, we need to convert the color space from RGB to BGR
+    cv2.imwrite(filename, cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
+    return jsonify({"filename" : filename})
+    
 if __name__ == "__main__":
-    time.sleep(5)  # Wait for the thread to start
-    model = Model(ModelLoader.load_model("yolov5s.onnx"))
-
-    while True:
-        try:
-            frame = stream_loader.load_frame()
-            preprocessed_frame, scale_factor = Preprocessor.preprocess(frame)
-            detected_objects = model.predict_and_get_detected_objects(
-                preprocessed_frame
-            )
-
-            annotator = Annotator(detected_objects, frame, scale_factor)
-            annotated_frame = annotator.annotate()
-
-            cv2.imshow("image", cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
-            cv2.waitKey(0)
-        except VideoCapError as e:
-            logger.error(e)
-
-        except Exception as e:
-            logger.error(e)
+    app.run(host="0.0.0.0", port=8080, debug=True)
