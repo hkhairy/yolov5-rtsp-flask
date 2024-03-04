3c750c9f66dd1c745e531ef784c28ae7a528421c
Hazem Khairy <hazem.hassan.kh@gmail.com>
2024-03-04 08:14:42 +0200

    feat: add Annotator class
    Now the detected objects are drawn on the original image

--- flask-app/main.py
+++ flask-app/main.py
@@ -5,6 +5,8 @@ import cv2
 from flask_app.stream_loader import RTSPOpenCVStreamLoader
 from flask_app.exceptions import VideoCapError
 from flask_app.yolov5 import Model, Preprocessor
+from flask_app.annotator import Annotator
+from flask import Flask
 
 # logging setup
 logger = logging.getLogger(__name__)
@@ -13,6 +15,8 @@ logging.basicConfig(level = logging.INFO)
 # stream loader setup
 #stream_loader = RTSPOpenCVStreamLoader(os.getenv("RTSP_URL"), os.getenv("THREAD_RETRY_INTERVAL"))
 stream_loader = RTSPOpenCVStreamLoader("rtsp://localhost:8554/stream", 5)
+
+
 
 
 if __name__ == "__main__":
@@ -25,8 +29,12 @@ if __name__ == "__main__":
             frame = stream_loader.load_frame()
             preprocessed_frame, scale_factor = Preprocessor.preprocess(frame)
             detected_objects = model.predict_and_get_detected_objects(preprocessed_frame)
-            print(detected_objects)
+
+            annotator = Annotator(detected_objects, frame, scale_factor)
+            annotated_frame = annotator.annotate()
             
+            cv2.imshow("image", cv2.cvtColor(annotated_frame, cv2.COLOR_RGB2BGR))
+            cv2.waitKey(0)
         except VideoCapError as e:
             logger.error(e)
             
---
+++ flask-app/src/flask_app/annotator.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+from nptyping import NDArray, Shape
+from flask_app.yolov5 import DetectedObject
+from typing import Any
+
+
+class Annotator:
+    """Annotator class
+    Takes an image and a list of detected objects and annotates the image with the detected objects
+    Inspired by https://github.com/ultralytics/ultralytics/blob/main/examples/YOLOv8-OpenCV-ONNX-Python/main.py
+    """
+    # The colors to use for the bounding boxes, and the text
+    colors_rgb = {
+        0: (255, 0, 0),
+        1: (0, 255, 0),
+        2: (0, 0, 255),
+        3: (255, 255, 0),
+        4: (0, 255, 255),
+        5: (255, 0, 255),
+        6: (255, 255, 255),
+        7: (0, 0, 0),
+        8: (128, 128, 128),
+        9: (128, 0, 0),
+        10: (128, 128, 0),
+        11: (0, 255, 0),
+        12: (0, 255, 255),
+        13: (0, 128, 128),
+        14: (0, 0, 128),
+        15: (255, 0, 255),
+        16: (128, 0, 128)
+    }
+    def __init__(
+            self, 
+            detected_objects: list[DetectedObject],
+            original_image: NDArray[Shape[Any,Any,3], np.uint8], 
+            scale_factor: float
+        ):
+        self.detected_objects = detected_objects
+        self.original_image = original_image
+        self.scale_factor = scale_factor
+
+    def annotate(self) -> NDArray[Shape["*,*,3"], np.uint8]:
+        """Returns the annotated image
+
+        Returns:
+            NDArray[Shape["*,*,3"], np.uint8]: The image after annotation
+        """
+        annotated_image = self.original_image.copy()
+        for detected_object in self.detected_objects:
+            x1, y1, x2, y2 = detected_object.box
+            x1 = int(x1 * self.scale_factor)
+            y1 = int(y1 * self.scale_factor)
+            x2 = int(x2 * self.scale_factor)
+            y2 = int(y2 * self.scale_factor)
+
+            color = Annotator.colors_rgb[detected_object.class_index % 16]
+            annotated_image = cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2)
+            annotated_image = cv2.putText(
+                annotated_image, 
+                detected_object.class_name, 
+                (x1, y1 - 10), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.5, 
+                color, 
+                2
+            )
+            
+        return annotated_image
+
